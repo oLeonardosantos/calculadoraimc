7c0994f2bb7b81bbf6d241ef40312e1b4cefd280
leonardo <leonardodossantosborges05@gmail.com>
2025-04-01 21:33:55 -0300

    Tela inicial editText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.comunidadedevspace.imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditTExt
+        // Criar uma variavél e associar o componente de UI<EditText>
+        // Recuperar o botao da tela
+
+        //Colocar a açao no botom
+        //Recuperar o texto digitado edtpeso
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btncalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btncalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+            println("leo acao do botao" + altura)
+        }
+
     }
 }
